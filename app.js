2ea63dcfc462d4ef30b4fc278390a3b76f6f056a
pambayungiri <pambayungiri1494@gmail.com>
2022-12-06 11:26:33 +0700

    342. let's take the todolist project to the next level and connect it with mongoose

--- app.js
+++ app.js
@@ -1,11 +1,46 @@
 const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
 const date = require(__dirname + "/date.js");
 const port = 3000;
 
-const items = ["buy food", "cook food", "eat food"];
-const workItems = [];
+// const items = ["buy food", "cook food", "eat food"];
+// const workItems = [];
+
+mongoose.set('strictQuery', true);
+
+mongoose.connect("mongodb://127.0.0.1:27017/todolistDB", {useNewUrlParser: true});
+
+const itemSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, "there's no name in it"],
+  }
+})
+
+const Item = mongoose.model("Item", itemSchema);
+
+const item1 = new Item({
+  name : "Welcome to your todolist!",
+})
+const item2 = new Item({
+  name : "Hit the + button to add a new item.",
+})
+const item3 = new Item({
+  name : "<-- Hit this to delete an item.",
+})
+
+const defaultItems = [item1, item2, item3];
+
+Item.insertMany(defaultItems, function (err) {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log("succesfully saved all the default items to todolistDB");
+  }
+})
+
 app.set("view engine", "ejs");
 
 app.use(bodyParser.urlencoded({ extended: true }));
